dd48c71d3bd749a3eb2456dae15f627b28ccdb71
Matt Bevilacqua <thewatermethod@gmail.com>
2022-10-14 20:29:25 -0400

    remove js (are we using it?)

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -1,48 +1,48 @@
-import 'focus-visible'
+// import 'focus-visible'
 
-document.documentElement.classList.remove('no-js')
+// document.documentElement.classList.remove('no-js')
 
-// Scroll State
-const onScroll = () => {
-    const scrollClassName = 'js-scrolled'
-    const scrollTreshold = 200
-    const isOverTreshold = window.scrollY > scrollTreshold
+// // Scroll State
+// const onScroll = () => {
+//     const scrollClassName = 'js-scrolled'
+//     const scrollTreshold = 200
+//     const isOverTreshold = window.scrollY > scrollTreshold
 
-    handleImageSwap(isOverTreshold)
+//     handleImageSwap(isOverTreshold)
 
-    if (isOverTreshold) {
-        document.documentElement.classList.add(scrollClassName)
-    } else {
-        document.documentElement.classList.remove(scrollClassName)
-    }
-}
-window.addEventListener('scroll', onScroll, { passive: true })
+//     if (isOverTreshold) {
+//         document.documentElement.classList.add(scrollClassName)
+//     } else {
+//         document.documentElement.classList.remove(scrollClassName)
+//     }
+// }
+// window.addEventListener('scroll', onScroll, { passive: true })
 
-// Print Button
-const printButton = document.querySelector('.js-print')
-printButton.addEventListener('click', () => {
-    window.print()
-})
+// // Print Button
+// const printButton = document.querySelector('.js-print')
+// printButton.addEventListener('click', () => {
+//     window.print()
+// })
 
-// handle image swap
-const handleImageSwap = (isOverTreshold) => {
-    // get image el
-    const img = document.querySelector('.vcard__avatar.u-photo')
-    const src = img.getAttribute('src')
+// // handle image swap
+// const handleImageSwap = (isOverTreshold) => {
+//     // get image el
+//     const img = document.querySelector('.vcard__avatar.u-photo')
+//     const src = img.getAttribute('src')
 
-    if (isOverTreshold) {
-        //smile
-        const smile = '/assets/images/smile.JPG'
-        if (src != smile) {
-            img.setAttribute('src', smile)
-        }
+//     if (isOverTreshold) {
+//         //smile
+//         const smile = '/assets/images/smile.JPG'
+//         if (src != smile) {
+//             img.setAttribute('src', smile)
+//         }
 
-        return
-    }
+//         return
+//     }
 
-    //frown
-    const frown = '/assets/images/frown.jpeg'
-    if (src != frown) {
-        img.setAttribute('src', frown)
-    }
-}
+//     //frown
+//     const frown = '/assets/images/frown.jpeg'
+//     if (src != frown) {
+//         img.setAttribute('src', frown)
+//     }
+// }
